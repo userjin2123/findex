c879382cd66ab0e3dfe82e15a4f1d5fcd68fbd21
userjin2123 <userjin2123@gmail.com>
2025-09-15 15:51:05 +0900

    N+1 문제 해제 이후 다시 해결을 위해 패치 조인 적용

--- src/main/java/com/example/findex/Test.java
+++ src/main/java/com/example/findex/Test.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 @Slf4j
 public class Test implements CommandLineRunner {
